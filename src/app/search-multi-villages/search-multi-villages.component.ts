12b7fa23ca08cf8c1065d2a5448cc2de8b97a051
Xiaoxin_He <xih85@pitt.edu>
2021-05-21 04:28:13 -0400

    get filter funct work , and select province, city in dropdown with filter

--- src/app/search-multi-villages/search-multi-villages.component.ts
+++ src/app/search-multi-villages/search-multi-villages.component.ts
@@ -1,4 +1,7 @@
+import { VillageNameService } from './../services/village-name.service';
 import { Component, OnInit } from '@angular/core';
+import { ProvinceCityCountyService } from '../services/province-city-county.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-search-multi-villages',
@@ -6,10 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-multi-villages.component.css']
 })
 export class SearchMultiVillagesComponent implements OnInit {
+  options;
+  provinceList: any[];
+  cityList: string[] = [];
+  displayedColumns: string[] = ['checked','name', 'province', 'city', 'county'];
+  dataSource;
+  selectedValue: string;
 
-  constructor() { }
+  constructor(private villageNameService: VillageNameService,
+    private provinceCityCountyService: ProvinceCityCountyService) {
+
+    this.provinceList = this.provinceCityCountyService.getProvince();
+
+  }
 
   ngOnInit(): void {
+    this.villageNameService.getVillages().then(result =>{
+      console.log(result);
+      console.log(result.data[69].province);
+      result.data.map(item =>{
+        if(this.cityList.includes(item.city) === false) {
+          this.cityList.push(item.city);
+        }
+      })
+      console.log(result.data[0].city);
+      console.log(this.cityList);
+      this.options = new MatTableDataSource(result.data);
+    })
+    console.log(this.provinceList);
   }
+
+
+
+  applyFilter(event: Event) {
+
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.options.filter = filterValue.trim().toLowerCase();
+    console.log(this.options.filter);
+  }
+
+  changeProvince(data: Event) {
+    this.options.filter = data;
+  }
+
+  changeCity(data: Event) {
+    this.options.filter = data;
+  }
+
 
 }
